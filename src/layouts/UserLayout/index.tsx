4165b80666f3d23ad5cbbeddb49c0a4b477f127d
Roman Usaev <roman.usaev@gmail.com>
2021-05-23 12:16:32 +0300

    add user data 24h

--- src/layouts/UserLayout/index.tsx
+++ src/layouts/UserLayout/index.tsx
@@ -1,10 +1,7 @@
-import React, { useState } from 'react';
-import classNames from 'classnames';
+import React from 'react';
 import styles from './user.module.scss';
 
 export function UserLayout() {
-
-    const [mouseTarget, setMouseTarget] = useState<number>(0);
     
       return (
         <>
@@ -13,30 +10,53 @@ export function UserLayout() {
                     <div className="content is-normal">
                         <h1 className={ styles.title }>Достижения</h1>
                         <table className={ styles.table }>
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>За все время</th>
+                                    <th>За 24 часа</th>
+                                </tr>
+                            </thead>
                             <tbody>
                                 <tr>
-                                    <td>Достигнутая высота</td>
+                                    <td><img className={ styles.icon } src="/images/summit.png" /> Достигнутая высота</td>
                                     <td>1745</td>
+                                    <td className={ styles.plus }>+17</td>
                                 </tr>
                                 <tr>
-                                    <td>Пройдено чекпоинтов</td>
+                                    <td><img className={ styles.icon } src="/images/flag-checkered.png" /> Пройдено чекпоинтов</td>
                                     <td>34</td>
+                                    <td className={ styles.plus }>+3</td>
                                 </tr>
                                 <tr>
-                                    <td>Изучено тем</td>
+                                    <td><img className={ styles.icon } src="/images/school-outline.png" /> Изучено тем</td>
                                     <td>12</td>
+                                    <td className={ styles.plus }>+1</td>
                                 </tr>
                                 <tr>
-                                    <td>Участие в соревнованиях</td>
+                                    <td><img className={ styles.icon } src="/images/hiking.png" /> Участие в соревнованиях</td>
                                     <td>315</td>
+                                    <td className={ styles.plus }>+6</td>
                                 </tr>
                                 <tr>
-                                    <td>Побед в соревнованиях</td>
-                                    <td>21</td>
+                                    <td><img className={ styles.icon } src="/images/seal.png" /> Побед в соревнованиях</td>
+                                    <td>46</td>
+                                    <td className={ styles.plus }>+2</td>
                                 </tr>
                                 <tr>
-                                    <th>Рейтинг</th>
+                                    <td><img className={ styles.icon } src="/images/seal.png" /> Процент побед</td>
+                                    <td>14%</td>
+                                    <td className={ styles.minus }>-7%</td>
+                                </tr>
+                                <tr>
+                                    <td><img className={ styles.icon } src="/images/map-check-outline.png" /> Наставничество</td>
+                                    <td>12</td>
+                                    <td className={ styles.plus }>+1</td>
+                                </tr>
+                                <tr>
+                                    <th><img className={ styles.icon } src="/images/podium-silver.png" /> Рейтинг</th>
                                     <th>2635</th>
+                                    <td className={ styles.plus }>+25</td>
                                 </tr>
                                             
                             </tbody>
